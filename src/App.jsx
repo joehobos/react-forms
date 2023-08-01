d4fe4c5c6e93f028fb005975c78e085644f37d22
joehobos <joehobos@DESKTOP-A520OBQ>
2023-07-31 17:33:45 -0700

    add css

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import Authenticate from "./components/Authenticate";
 import SignUpForm from "./components/SignUpForm";
 import React from "react";
 import { useState } from "react";
+import './App.css'
 
 function App() {
   const [token, setToken] = useState(null)
 
 
   return (
-    <>
-      <Authenticate token={token} setToken={setToken} />
+    <div>
       <SignUpForm token={token} setToken={setToken}/>
-    </>
+      <Authenticate token={token} setToken={setToken} />
+    </div>
   );
 }
 
